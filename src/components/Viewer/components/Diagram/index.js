3fb2e2223c7cdc42f752754949d71ba6eab6eae6
Jacob Windsor <jacob.windsor@ymail.com>
2017-06-11 17:19:56 +0200

    Make diagram stateless

--- src/components/Viewer/components/Diagram/index.js
+++ src/components/Viewer/components/Diagram/index.js
@@ -4,49 +4,42 @@ import './index.css';
 import PropTypes from 'prop-types';
 import { isMatch, difference } from 'lodash';
 
-class Diagram extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+const Diagram = (props) => {
 
-    onPvjsReady = (pvjsRef) => {
-        const { onReady } = this.props;
-        this.setState({manipulator: pvjsRef.manipulator});
+    const onPvjsReady = (pvjsRef) => {
+        const { onReady } = props;
         onReady();
     }
 
-    render() {
-        const { wpId, version, showPanZoomControls, isHidden, slide } = this.props;
-        const targets = slide.targets;
+    const { wpId, version, showPanZoomControls, isHidden, slide } = props;
+    const targets = slide.targets;
 
-        const getEntityIds = entities => entities.map(singleEntity => singleEntity.entityId);
+    const getEntityIds = entities => entities.map(singleEntity => singleEntity.entityId);
 
-        const highlightedEntities = targets
-            .filter(singleTarget => singleTarget.highlighted)
-            .map(singleTarget => Object.assign({},
-                {entityId: singleTarget.entityId, color: singleTarget.highlightedColor})
-            );
-        const pannedEntities = getEntityIds(targets
-            .filter(singleTarget => singleTarget.panned));
-        const zoomedEntities = getEntityIds(targets
-            .filter(singleTarget => singleTarget.zoomed));
-        const hiddenEntities = getEntityIds(targets
-            .filter(singleTarget => singleTarget.hidden));
+    const highlightedEntities = targets
+        .filter(singleTarget => singleTarget.highlighted)
+        .map(singleTarget => Object.assign({},
+            {entityId: singleTarget.entityId, color: singleTarget.highlightedColor})
+        );
+    const pannedEntities = getEntityIds(targets
+        .filter(singleTarget => singleTarget.panned));
+    const zoomedEntities = getEntityIds(targets
+        .filter(singleTarget => singleTarget.zoomed));
+    const hiddenEntities = getEntityIds(targets
+        .filter(singleTarget => singleTarget.hidden));
 
-        return (
-            <div className={`diagram-wrapper ${isHidden? 'isHidden' : ''}`}>
-                <Pvjs about={`http://identifiers.org/wikipathways/WP${wpId}`}
-                      version={version}
-                      showPanZoomControls={showPanZoomControls}
-                      highlightedEntities={highlightedEntities}
-                      pannedEntities={pannedEntities}
-                      zoomedEntities={zoomedEntities}
-                      hiddenEntities={hiddenEntities}
-                      onReady={this.onPvjsReady} />
-            </div>
-        )
-    }
+    return (
+        <div className={`diagram-wrapper ${isHidden? 'isHidden' : ''}`}>
+            <Pvjs about={`http://identifiers.org/wikipathways/WP${wpId}`}
+                  version={version}
+                  showPanZoomControls={showPanZoomControls}
+                  highlightedEntities={highlightedEntities}
+                  pannedEntities={pannedEntities}
+                  zoomedEntities={zoomedEntities}
+                  hiddenEntities={hiddenEntities}
+                  onReady={onPvjsReady} />
+        </div>
+    )
 }
 
 Diagram.propTypes = {
