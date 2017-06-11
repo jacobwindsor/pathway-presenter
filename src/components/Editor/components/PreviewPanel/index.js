46aaf5e20fb03e451e4c9fe13ea56b477bf4a6e8
Jacob Windsor <jacob.windsor@ymail.com>
2017-06-11 18:07:39 +0200

    Show static slides

--- src/components/Editor/components/PreviewPanel/index.js
+++ src/components/Editor/components/PreviewPanel/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './index.css';
+import StaticSlide from './components/StaticSlide';
 
 const PreviewPanel = (props) => {
-    const { slides } = props;
+    const { slides, wpId, version } = props;
+
+    const staticSlides = slides
+        .map(singleSlide => <StaticSlide slide={singleSlide} wpId={wpId} version={version} />);
 
     return (
         <div className="preview-panel">
-
+            {{staticSlides}}
         </div>
     )
 };
 
 PreviewPanel.propTypes = {
-    slides: PropTypes.array.isRequired
+    slides: PropTypes.array.isRequired,
+    wpId: PropTypes.string.isRequired,
+    version: PropTypes.number.isRequired,
 };
 
 export default PreviewPanel;
