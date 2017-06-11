da2614a96f6e78133ae6e7441557a470dc27fde0
Jacob Windsor <jacob.windsor@ymail.com>
2017-06-11 18:16:43 +0200

    Add logic to getting the presentation to the editor

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -2,10 +2,42 @@ import './index.css';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Viewer from '../Viewer';
+import presentations from '../../data/presentations';
+import PreviewPanel from './components/PreviewPanel';
 
 class Editor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            presentation: null,
+            loading: true,
+            activeSlideIndex: 0,
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        const { presId } = this.props;
+        presentations.get(presId)
+            .then(presentation => {
+                this.setState({
+                    presentation,
+                    loading: false,
+                    activeSlideIndex: 0,
+                })
+            })
+            .catch(err => {
+                this.setState({
+                    error: err,
+                    loading: false,
+                })
+            })
+    }
+
     render() {
         const { presId } = this.props;
+        const { loading, error, presentation } = this.state;
+
         return (
             <div className="editor-wrapper">
                 <Viewer presId={presId}/>
