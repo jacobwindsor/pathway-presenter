27ec29e2c2aa1effae2379be58575aea04648875
Jacob Windsor <jacob.windsor@ymail.com>
2017-06-27 10:20:04 +0200

    Use a placehloder for previews

--- src/components/Editor/components/Creator/components/PreviewPanel/components/StaticSlide/index.js
+++ src/components/Editor/components/Creator/components/PreviewPanel/components/StaticSlide/index.js
@@ -1,41 +1,18 @@
 import React from 'react';
 import './index.css';
+import board from '../../../../../../../../assets/board_64.svg';
 import PropTypes from 'prop-types';
-import { Pvjs } from '@wikipathways/pvjs';
 
 const StaticSlide = (props) => {
-    const { slide, wpId, version, slideNumber, onClick } = props;
+    const { slideNumber, onClick } = props;
 
     const handleClick = () => {
         onClick(slideNumber)
     };
 
-    const targets = slide.targets;
-
-    const getEntityIds = entities => entities.map(singleEntity => singleEntity.entityId);
-
-    const highlightedEntities = targets
-        .filter(singleTarget => singleTarget.highlighted)
-        .map(singleTarget => Object.assign({},
-            {entityId: singleTarget.entityId, color: singleTarget.highlightedColor})
-        );
-    const pannedEntities = getEntityIds(targets
-        .filter(singleTarget => singleTarget.panned));
-    const zoomedEntities = getEntityIds(targets
-        .filter(singleTarget => singleTarget.zoomed));
-    const hiddenEntities = getEntityIds(targets
-        .filter(singleTarget => singleTarget.hidden));
-
     return (
         <div className="static-slide" onClick={handleClick}>
-            <Pvjs wpId={wpId}
-                  version={version}
-                  showPanZoomControls={false}
-                  highlightedEntities={highlightedEntities}
-                  pannedEntities={pannedEntities}
-                  zoomedEntities={zoomedEntities}
-                  hiddenEntities={hiddenEntities}
-            />
+            <img src={board} style={{margin: '0 auto', display: 'block', transform: 'translateY(50%)', top: '50%'}} />
             <div className="title">
                 {slideNumber}
             </div>
