6a37ea6095350caacd97ff0635fd4aecb8b8261f
Jacob Windsor <jacob.windsor@ymail.com>
2017-06-27 15:54:07 +0200

    Remove console log

--- src/components/Editor/components/Creator/index.js
+++ src/components/Editor/components/Creator/index.js
@@ -92,7 +92,6 @@ class Creator extends Component {
     render() {
         const { activeSlideIndex, selectedEntity, presentation } = this.state;
         const slide = presentation.slides[activeSlideIndex];
-        console.log(activeSlideIndex);
 
         return (
             <div className="creator-wrapper">
