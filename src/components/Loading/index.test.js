f06b4a6ad7dd009fa9dce747a002390b670ddb4b
Jacob Windsor <jacob.windsor@ymail.com>
2017-07-05 13:46:06 +0200

    Add snapshot to Loading

--- src/components/Loading/index.test.js
+++ src/components/Loading/index.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Loading from './index';
+import toJson from 'enzyme-to-json';
 
 it('renders without failing', () => {
-  shallow(<Loading />);
+  const comp = shallow(<Loading />);
+  expect(toJson(comp)).toMatchSnapshot();
 });
