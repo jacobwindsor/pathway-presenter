ae5dc845d115f04e85fa3636f0889164a89f27d0
Jacob Windsor <jacob.windsor@ymail.com>
2017-08-03 18:04:56 +0100

    add alpha notice

--- src/components/Editor/components/Adder/index.js
+++ src/components/Editor/components/Adder/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Paper from 'material-ui/Paper';
-import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 import AutoComplete from 'material-ui/AutoComplete';
 import presentations from '../../../../data/presentations';
@@ -9,7 +8,8 @@ import { cloneDeep } from 'lodash';
 import LogoWhite from '../../../../assets/logo-white.svg';
 import './index.css';
 import Formsy from 'formsy-react';
-import { FormsyText, FormsyAutoComplete } from 'formsy-material-ui/lib';
+import { FormsyText } from 'formsy-material-ui/lib';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 class Adder extends Component {
   constructor(props) {
@@ -74,101 +74,113 @@ class Adder extends Component {
     };
 
     return (
-      <Paper zDepth={1} className="adder-wrapper">
-        <div className="header">
-          <h1>Pathway Presenter</h1>
-          <p>
-            Create interactive presentations from pathways on{' '}
-            <a
-              href="http://wikipathways.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              WikiPathways
-            </a>.
-          </p>
-          <div className="logo-wrapper">
-            <img src={LogoWhite} alt="Logo" />
+      <Scrollbars>
+        <Paper zDepth={1} className="adder-wrapper">
+          <div className="header">
+            <h1>Pathway Presenter</h1>
+            <p>
+              Create interactive presentations from pathways on{' '}
+              <a
+                href="http://wikipathways.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                WikiPathways
+              </a>.
+            </p>
+            <div className="logo-wrapper">
+              <img src={LogoWhite} alt="Logo" />
+            </div>
           </div>
-        </div>
-        <div className="content">
-          <h3>Select a Pathway Presentation</h3>
-          <AutoComplete
-            filter={AutoComplete.fuzzyFilter}
-            maxSearchResults={5}
-            fullWidth={true}
-            openOnFocus={true}
-            hintText={'Search by title'}
-            dataSource={this.state.dataSource}
-            onNewRequest={this.handleSelect}
-          />
-          <Formsy.Form
-            onValid={() => this.setState({ canSubmit: true })}
-            onInvalid={() => this.setState({ canSubmit: false })}
-            onValidSubmit={this.handleSubmit}
-            onInvalidSubmit={() => null}
-          >
-            <h3 id="creator-form-title">Create a Pathway Presentation</h3>
-            <FormsyText
-              name="title"
-              required
-              requiredError={errorMessages.title}
-              hintText="The TCA Cycle"
-              floatingLabelText="Title"
-              fullWidth
-              updateImmediately
-            />
-            <FormsyText
-              required
-              name="authorName"
-              requiredError={'You must fill this in'}
-              validationError={errorMessages.authorName}
-              hintText="Gregor Mendel, Frederick Sanger"
-              floatingLabelText="Author Name(s)"
-              fullWidth
-              updateImmediately
-            />
-            <FormsyText
-              name="wpId"
-              required
-              validationError={errorMessages.wpId}
-              validationErrors={{
-                beginsWithWP:
-                  'The WikiPathways ID must begin with WP followed by a number'
-              }}
-              validations={{
-                beginsWithWP: (values, value) => {
-                  const beginning = value.slice(0, 2);
-                  const end = value.slice(2);
-                  return beginning === 'WP' && !isNaN(end);
-                }
-              }}
-              hintText="WP78"
-              floatingLabelText="WikiPathways ID"
-              value={''}
-              fullWidth
-              updateImmediately
-            />
-            <FormsyText
-              name="version"
-              validations="isNumeric"
-              validationError={errorMessages.version}
-              hintText="Type '0' for latest"
-              floatingLabelText="WikiPathways Version (optional)"
-              fullWidth
-              updateImmediately
-            />
-            <FlatButton
-              label="Create"
-              primary={true}
-              type="submit"
+          <div className="content">
+            <Paper id="alpha-notice">
+              <h3>Alpha Version</h3>
+              <p>
+                The Pathway Presenter is currently in alpha stage. This means
+                that any presentations you create will only be saved locally.
+                You cannot share them and can only present via the browser used
+                to create the presentation. This will be rectified for the beta
+                version.
+              </p>
+            </Paper>
+            <h3>Select a Pathway Presentation</h3>
+            <AutoComplete
+              filter={AutoComplete.fuzzyFilter}
+              maxSearchResults={5}
               fullWidth={true}
-              className="create-button"
-              disabled={!this.state.canSubmit}
+              openOnFocus={true}
+              hintText={'Search by title'}
+              dataSource={this.state.dataSource}
+              onNewRequest={this.handleSelect}
             />
-          </Formsy.Form>
-        </div>
-      </Paper>
+            <Formsy.Form
+              onValid={() => this.setState({ canSubmit: true })}
+              onInvalid={() => this.setState({ canSubmit: false })}
+              onValidSubmit={this.handleSubmit}
+              onInvalidSubmit={() => null}
+            >
+              <h3 id="creator-form-title">Create a Pathway Presentation</h3>
+              <FormsyText
+                name="title"
+                required
+                requiredError={errorMessages.title}
+                hintText="The TCA Cycle"
+                floatingLabelText="Title"
+                fullWidth
+                updateImmediately
+              />
+              <FormsyText
+                required
+                name="authorName"
+                requiredError={'You must fill this in'}
+                validationError={errorMessages.authorName}
+                hintText="Gregor Mendel, Frederick Sanger"
+                floatingLabelText="Author Name(s)"
+                fullWidth
+                updateImmediately
+              />
+              <FormsyText
+                name="wpId"
+                required
+                validationError={errorMessages.wpId}
+                validationErrors={{
+                  beginsWithWP:
+                    'The WikiPathways ID must begin with WP followed by a number'
+                }}
+                validations={{
+                  beginsWithWP: (values, value) => {
+                    const beginning = value.slice(0, 2);
+                    const end = value.slice(2);
+                    return beginning === 'WP' && !isNaN(end);
+                  }
+                }}
+                hintText="WP78"
+                floatingLabelText="WikiPathways ID"
+                value={''}
+                fullWidth
+                updateImmediately
+              />
+              <FormsyText
+                name="version"
+                validations="isNumeric"
+                validationError={errorMessages.version}
+                hintText="Type '0' for latest"
+                floatingLabelText="WikiPathways Version (optional)"
+                fullWidth
+                updateImmediately
+              />
+              <FlatButton
+                label="Create"
+                primary={true}
+                type="submit"
+                fullWidth={true}
+                className="create-button"
+                disabled={!this.state.canSubmit}
+              />
+            </Formsy.Form>
+          </div>
+        </Paper>
+      </Scrollbars>
     );
   }
 }
